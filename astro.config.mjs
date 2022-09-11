73cc3725c7131246a71def0baa7c35d63b89522a
Tiep Phan <tieppt12@gmail.com>
2022-09-11 14:49:02 +0700

    add hyperlink icon for heading

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
         [
           rehypeAutolinkHeadings,
           {
-            behavior: 'wrap',
+            behavior: 'append',
           },
         ],
         [
